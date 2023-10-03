44935b377c4174d651fc69ed405a0de71275cd86
Bhashana <bhashanaliyanage@gmail.com>
2023-10-04 00:29:29 +0530

    Search update 2

--- src/main/java/com/example/song_finder_fx/HelloController.java
+++ src/main/java/com/example/song_finder_fx/HelloController.java
@@ -3,7 +3,6 @@ package com.example.song_finder_fx;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
 
 import java.io.File;
