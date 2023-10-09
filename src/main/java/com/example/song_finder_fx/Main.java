c159c66ac95510bf9be39a940a88a19a96dca93f
Bhashana <bhashanaliyanage@gmail.com>
2023-10-09 19:56:36 +0530

    Optimized imports

--- src/main/java/com/example/song_finder_fx/Main.java
+++ src/main/java/com/example/song_finder_fx/Main.java
@@ -1,21 +1,22 @@
 package com.example.song_finder_fx;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import javax.swing.*;
 import java.awt.*;
+import java.beans.EventHandler;
 import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 
 public class Main extends Application {
-
-
 
     public static void main(String[] args) {
         new Thread(() -> launch(args)).start();
@@ -37,6 +38,10 @@ public class Main extends Application {
         stage.getIcons().add(image);
 
         stage.show();
+
+        stage.setOnCloseRequest(e -> {
+            Platform.exit();
+        });
     }
 
     public File browseFile() {
@@ -65,6 +70,7 @@ public class Main extends Application {
         nb.displayTray();
         return "Done";
     }
+
     public File browseDestination() {
         JFileChooser chooser = new JFileChooser();
         chooser.setDialogTitle("Choose a directory");
