9d51d7aef278c92cca8546819edf0a6a8a428d43
witchx44@gmail.com <witchx44@gmail.com>
2024-01-31 15:50:27 +0530

    Added: Partner Share of Gross, Amount Payable, Tax to Artist ReportPDF

--- src/main/java/com/example/song_finder_fx/Model/ArtistReport.java
+++ src/main/java/com/example/song_finder_fx/Model/ArtistReport.java
@@ -3,38 +3,43 @@ package com.example.song_finder_fx.Model;
 import java.text.DecimalFormat;
 
 public class ArtistReport {
-    private String payee;
-    private String grossRevenueInLKR;
-    private String partnerShareInLKR;
-    private String taxAmount;
-    private String amountPayable;
+    private String payee = "";
+    private String grossRevenueInLKR = "";
+    private String partnerShareInLKR = "";
+    private String taxAmount = "0";
+    private String amountPayable = "";
+    private String month = "";
     private final DecimalFormat df = new DecimalFormat("0.00");
 
-    public void setGrossRevenue(String selectedItem, double grossRevenueInLKR, double partnerShareInLKR, double tax, double amountPayable) {
+    public void setGrossRevenue(String selectedItem, double grossRevenueInLKR, double partnerShareInLKR, double tax, double amountPayable, String month) {
         this.payee = selectedItem;
         this.grossRevenueInLKR = df.format(grossRevenueInLKR);
         this.partnerShareInLKR = df.format(partnerShareInLKR);
         this.taxAmount = df.format(tax);
         this.amountPayable = df.format(amountPayable);
+        this.month = month;
     }
 
     public String getGrossRevenueInLKR() {
-        return grossRevenueInLKR;
+        return "LKR " + grossRevenueInLKR;
     }
 
     public String getPartnerShareInLKR() {
-        return partnerShareInLKR;
+        return "LKR " + partnerShareInLKR;
     }
 
     public String getTaxAmount() {
-        return taxAmount;
+        return "LKR " + taxAmount;
     }
 
     public String getAmountPayable() {
-        return amountPayable;
+        return "LKR " + amountPayable;
     }
 
     public String getPayee() {
         return payee;
+    }
+    public String getMonth() {
+        return month;
     }
 }
