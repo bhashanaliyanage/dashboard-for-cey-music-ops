5b8662af329315b6c472bb8f688f0e8dc06f6b12
sudeshSandruwan <62739217+sudeshSandaruwan@users.noreply.github.com>
2024-06-12 12:43:03 +0530

    revenue report modification create service to get missing songs and isrcPayees

--- src/main/java/com/example/song_finder_fx/Controller/IngestController.java
+++ src/main/java/com/example/song_finder_fx/Controller/IngestController.java
@@ -4,7 +4,11 @@ import com.example.song_finder_fx.DatabasePostgres;
 import com.example.song_finder_fx.Model.Ingest;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class IngestController {
@@ -12,4 +16,50 @@ public class IngestController {
     public List<Ingest> getCreatedIngests() throws SQLException, IOException {
         return DatabasePostgres.getAllIngests();
     }
+
+        public List<String> getMissingPayeeList() {
+
+        List<String> stringList = new ArrayList<>();
+            Connection con = DatabasePostgres.getConn();
+            List<String> isrcList = new ArrayList<String>();
+            String sql = "select asset_isrc FROM report WHERE asset_isrc NOT IN(SELECT isrc FROM isrc_payees )";
+            try {
+                PreparedStatement ps = con.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    String isrc = rs.getString(1);
+                    isrcList.add(isrc);
+
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                DatabasePostgres.closeConnection(con);
+            }
+            return isrcList;
+        }
+
+        public List<String>  getMissingSongs(){
+
+            List<String> isrcList = new ArrayList<String>();
+            Connection con = DatabasePostgres.getConn();
+            String sql = "SELECT asset_isrc FROM report WHERE asset_isrc NOT IN (SELECT isrc FROM songs)";
+            try {
+                PreparedStatement ps = con.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+
+                while (rs.next()) {
+                    String isrc = rs.getString(1);
+                    isrcList.add(isrc);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                DatabasePostgres.closeConnection(con);
+            }
+            return isrcList;
+
+        }
+
 }
